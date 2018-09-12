0a3da7f90ab08f7b6778a5584b9213ea225b8fb1
jorgemayna <jorgemayna@gmail.com>
2018-09-12 18:03:59 -0500

    vector final

--- main.cpp
+++ main.cpp
@@ -6,9 +6,23 @@
 using namespace std;
 
 template <typename T>
-struct Dimensions {      
+struct Dimensions {
     int operator()(int* coordinates, int* sizes, int dimensions) {
         // TODO
+        int pos=0;
+        for(int i=0;i<dimensions;i++)
+        {
+            int po=1;
+            for(int j=i+1;j<dimensions;j++)
+            {
+                po=po*sizes[j];
+            }
+            po=po*coordinates[i];
+            pos=pos+po;
+        }
+
+
+        return pos;
     }
 };
 
@@ -18,6 +32,46 @@ struct Integer {
 };
 
 int main(int argc, char *argv[]) {
+
+    //Cree una matriz 2x3 para corroborar el funsionamiento.
+    int *cor;
+    int *dim;
+    cor=new int[2];
+    dim=new int[2];
+    dim[0]=2;
+    dim[1]=3;
+    cor[0]=0;
+    cor[1]=0;
+
+    int tt=1;
+    Vector<Integer> vector(2,dim);
+    for(int i=0;i<2;i++)
+    {
+
+        for(int j=0;j<3;j++)
+        {
+            cor[0]=i;
+            cor[1]=j;
+            vector.set(tt,cor);
+            tt++;
+        }
+    }
+
+
+
+    for(int i=0;i<2;i++)
+    {
+
+        for(int j=0;j<3;j++)
+        {
+            cor[0]=i;
+            cor[1]=j;
+            cout <<vector.get(cor)<<" ";
+
+        }
+        cout<<endl;
+    }
+
     system("pause");
     return EXIT_SUCCESS;
 }
--- vector.h
+++ vector.h
@@ -1,29 +1,49 @@
 #ifndef VECTOR_H
 #define VECTOR_H
-
+#include<iostream>
+using namespace std;
 template <typename Tr>
 class Vector {
     public:
         typedef typename Tr::T T;
         typedef typename Tr::Operation Operation;
-             
+
     private:
         T* data;
         int dataSize;
+        Operation op;
 
         int dimensions;
         int* dimensionSizes;
 
     public:
         Vector() : data(nullptr) {};
-             
+
         Vector(int dimensions, int* dimensionSizes) : dimensions(dimensions), dimensionSizes(dimensionSizes) {
             // TODO
+
+            int p=1;
+            for(int i=0;i<dimensions;i++)
+            {
+                p=p*dimensionSizes[i];
+            }
+
+
+            data=new T[p];
+
         }
-             
-        void set(T datum, int* coordinates); // TODO
-             
-        T get(int* coordinates); // TODO
+
+        void set(T datum, int* coordinates)
+        {
+            data[op(coordinates,dimensionSizes,dimensions)]=datum;
+        }; // TODO
+
+        T get(int* coordinates)
+        {
+
+            return data[op(coordinates,dimensionSizes,dimensions)];
+
+        }; // TODO
 };
 
 #endif
